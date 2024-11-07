8ab79903d87f995fb6d08ba1f1b7df5c7cc20f76
João Mesquita <jolitas2005@gmail.com>
2024-11-07 16:37:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Equipa2-Incremento2
+ Programming Laboratory Project - Team 2 - Increment 2 (2024/2025)
---
+++ src/main/java/Equipa2/Incremento2/Incremento2Application.java
@@ -0,0 +1,13 @@
+package Equipa2.Incremento2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Incremento2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Incremento2Application.class, args);
+	}
+
+}

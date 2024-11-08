56a55763cd3c0598a84512a64e2bc50813c67595
Devysson Cardoso <devyssonsc@gmail.com>
2024-11-08 10:22:21 +0000

    Create AvaliacaoRepository.java

---
+++ src/main/java/Equipa2/Incremento2/repository/AvaliacaoRepository.java
@@ -0,0 +1,11 @@
+package Equipa2.Incremento2.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import Equipa2.Incremento2.model.Avaliacao;
+
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, UUID> {
+    
+}

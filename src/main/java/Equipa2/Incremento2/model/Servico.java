c0f417c9e9bcb4b85f10969bbec3eb2d8eae5b44
Devysson Cardoso <devyssonsc@gmail.com>
2024-11-08 09:57:47 +0000

    Create Servico.java

---
+++ src/main/java/Equipa2/Incremento2/model/Servico.java
@@ -0,0 +1,31 @@
+package Equipa2.Incremento2.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Servico")
+public class Servico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String titulo;
+
+    private String descricao;
+    private LocalDateTime data;
+    private double valorHora;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "profissional_id")
+    private Profissional profissional;
+}

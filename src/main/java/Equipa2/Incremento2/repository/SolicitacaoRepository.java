7045e35cd17a9ec873d8aae1aece518ce0e47fad
João Mesquita <151409184+jfgmesquita@users.noreply.github.com>
2024-11-08 11:18:28 +0000

    @Repository SolicitacaoRepository.java

--- src/main/java/Equipa2/Incremento2/repository/SolicitacaoRepository.java
+++ src/main/java/Equipa2/Incremento2/repository/SolicitacaoRepository.java
@@ -5,5 +5,5 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import Equipa2.Incremento2.model.Solicitacao;
-
+@Repository
 public interface SolicitacaoRepository extends JpaRepository<Solicitacao, UUID> {}

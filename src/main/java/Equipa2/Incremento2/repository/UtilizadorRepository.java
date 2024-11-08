c49ae43147cfaf61e5f59b89b91f8eb940fd3413
João Mesquita <151409184+jfgmesquita@users.noreply.github.com>
2024-11-08 11:18:47 +0000

    @Repository UtilizadorRepository.java

--- src/main/java/Equipa2/Incremento2/repository/UtilizadorRepository.java
+++ src/main/java/Equipa2/Incremento2/repository/UtilizadorRepository.java
@@ -5,5 +5,5 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import Equipa2.Incremento2.model.Utilizador;
-
+@Repository
 public interface UtilizadorRepository extends JpaRepository<Utilizador, UUID> {}

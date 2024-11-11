f26d4e9a3a8bb9840a09b43bbc54082b94a5fef8
João Mesquita <jolitas2005@gmail.com>
2024-11-11 16:45:10 +0000

    Import repository

--- src/main/java/Equipa2/Incremento2/repository/ServicoRepository.java
+++ src/main/java/Equipa2/Incremento2/repository/ServicoRepository.java
@@ -3,6 +3,7 @@ package Equipa2.Incremento2.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import Equipa2.Incremento2.model.Servico;
 @Repository

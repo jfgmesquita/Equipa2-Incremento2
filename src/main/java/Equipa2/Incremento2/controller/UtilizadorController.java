fe43083b5c15c180309046d67116a2100dbcc4d8
Devysson Cardoso <devyssonsc@gmail.com>
2024-11-17 21:15:42 +0000

    Update UtilizadorController.java

--- src/main/java/Equipa2/Incremento2/controller/UtilizadorController.java
+++ src/main/java/Equipa2/Incremento2/controller/UtilizadorController.java
@@ -3,6 +3,11 @@ package Equipa2.Incremento2.controller;
 import java.util.List;
 import java.util.UUID;
 
+import Equipa2.Incremento2.model.Admin;
+import Equipa2.Incremento2.model.Cliente;
+import Equipa2.Incremento2.model.Profissional;
+import Equipa2.Incremento2.model.dto.UtilizadorDTO;
+import Equipa2.Incremento2.model.enums.UserType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +17,7 @@ import Equipa2.Incremento2.service.UtilizadorService;
 
 @RestController
 @RequestMapping("/api/utilizadores")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UtilizadorController {
     @Autowired
     private UtilizadorService utilizadorService;
@@ -33,26 +39,97 @@ public class UtilizadorController {
     }
 
     @PostMapping
-    public Utilizador createUtilizador(@RequestBody Utilizador utilizador) {
-        return utilizadorService.save(utilizador);
+    public ResponseEntity<Utilizador> createUtilizador(@RequestBody UtilizadorDTO utilizador){
+
+        if(utilizador.getUserType() == UserType.CLIENTE){
+            Cliente cliente = new Cliente();
+            cliente.setNome(utilizador.getNome());
+            cliente.setEmail(utilizador.getEmail());
+            cliente.setPassword(utilizador.getPassword());
+            cliente.setMorada(utilizador.getMorada());
+            cliente.setUserType(utilizador.getUserType());
+            cliente.setFormaDePagamento(utilizador.getFormaDePagamento());
+            utilizadorService.saveCliente(cliente);
+            return ResponseEntity.ok().body(cliente);
+        }
+
+        if(utilizador.getUserType() == UserType.PROFISSIONAL){
+            Profissional profissional = new Profissional();
+            profissional.setNome(utilizador.getNome());
+            profissional.setEmail(utilizador.getEmail());
+            profissional.setPassword(utilizador.getPassword());
+            profissional.setMorada(utilizador.getMorada());
+            profissional.setUserType(utilizador.getUserType());
+            profissional.setFormaDePagamento(utilizador.getFormaDePagamento());
+            profissional.setEspecialidade(utilizador.getEspecialidade());
+            profissional.setExperiencia(utilizador.getExperiencia());
+            utilizadorService.saveProfissional(profissional);
+            return ResponseEntity.ok().body(profissional);
+        }
+
+        if(utilizador.getUserType() == UserType.ADMINISTRADOR){
+            Admin admin = new Admin();
+            admin.setNome(utilizador.getNome());
+            admin.setEmail(utilizador.getEmail());
+            admin.setPassword(utilizador.getPassword());
+            admin.setMorada(utilizador.getMorada());
+            admin.setUserType(utilizador.getUserType());
+            admin.setCodigo(utilizador.getCodigo());
+            utilizadorService.saveAdmin(admin);
+            return ResponseEntity.ok().body(admin);
+        }
+
+        return ResponseEntity.badRequest().build();
+
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Utilizador> updateUtilizador(@PathVariable UUID id, @RequestBody Utilizador utilizadorDetails) {
+    public ResponseEntity<Utilizador> updateUtilizador(@PathVariable UUID id, @RequestBody UtilizadorDTO utilizadorDetails) {
         Utilizador utilizador = utilizadorService.findById(id);
-        
+
         if (utilizador == null) {
             return ResponseEntity.notFound().build();
         }
 
-        utilizador.setNome(utilizadorDetails.getNome());
-        utilizador.setEmail(utilizadorDetails.getEmail());
-        utilizador.setPassword(utilizadorDetails.getPassword());
-        utilizador.setMorada(utilizadorDetails.getMorada());
-        utilizador.setUserType(utilizadorDetails.getUserType());
-        Utilizador updatedUtilizador = utilizadorService.save(utilizador);
+        if(utilizador.getUserType() == UserType.CLIENTE){
+            Cliente cliente = (Cliente) utilizador;
+            cliente.setNome(utilizadorDetails.getNome());
+            cliente.setEmail(utilizadorDetails.getEmail());
+            cliente.setPassword(utilizadorDetails.getPassword());
+            cliente.setMorada(utilizadorDetails.getMorada());
+            cliente.setUserType(utilizadorDetails.getUserType());
+            cliente.setFormaDePagamento(utilizadorDetails.getFormaDePagamento());
+            utilizadorService.saveCliente(cliente);
+            return ResponseEntity.ok().body(cliente);
+        }
 
-        return ResponseEntity.ok(updatedUtilizador);
+        if(utilizador.getUserType() == UserType.PROFISSIONAL){
+            Profissional profissional = (Profissional) utilizador;
+            profissional.setNome(utilizadorDetails.getNome());
+            profissional.setEmail(utilizadorDetails.getEmail());
+            profissional.setPassword(utilizadorDetails.getPassword());
+            profissional.setMorada(utilizadorDetails.getMorada());
+            profissional.setUserType(utilizadorDetails.getUserType());
+            profissional.setFormaDePagamento(utilizadorDetails.getFormaDePagamento());
+            profissional.setEspecialidade(utilizadorDetails.getEspecialidade());
+            profissional.setExperiencia(utilizadorDetails.getExperiencia());
+            utilizadorService.saveProfissional(profissional);
+            return ResponseEntity.ok().body(profissional);
+        }
+
+        if(utilizador.getUserType() == UserType.ADMINISTRADOR){
+            Admin admin = (Admin) utilizador;
+            admin.setNome(utilizadorDetails.getNome());
+            admin.setEmail(utilizadorDetails.getEmail());
+            admin.setPassword(utilizadorDetails.getPassword());
+            admin.setMorada(utilizadorDetails.getMorada());
+            admin.setUserType(utilizadorDetails.getUserType());
+            admin.setCodigo(utilizadorDetails.getCodigo());
+            utilizadorService.saveAdmin(admin);
+            return ResponseEntity.ok().body(admin);
+        }
+
+        return ResponseEntity.badRequest().build();
     }
 
     @DeleteMapping("/{id}")

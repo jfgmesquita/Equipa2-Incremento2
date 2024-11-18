72c05d0f050a00551e5c9a7d08b04d8d07d0adb5
Devysson Cardoso <devyssonsc@gmail.com>
2024-11-18 17:55:51 +0000

    Create ServicoDTO.java

---
+++ src/main/java/Equipa2/Incremento2/model/dto/ServicoDTO.java
@@ -0,0 +1,25 @@
+package Equipa2.Incremento2.model.dto;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServicoDTO {
+
+    private String titulo;
+
+    private String descricao;
+    private String data;
+    private double valorHora;
+
+    private UUID profissional;
+}

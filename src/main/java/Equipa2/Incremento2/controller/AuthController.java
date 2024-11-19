ad00d3e988902d9e16fc99ecab1d3c176613b685
Devysson Cardoso <devyssonsc@gmail.com>
2024-11-19 01:24:55 +0000

    Update AuthController.java

--- src/main/java/Equipa2/Incremento2/controller/AuthController.java
+++ src/main/java/Equipa2/Incremento2/controller/AuthController.java
@@ -3,6 +3,7 @@ package Equipa2.Incremento2.controller;
 import Equipa2.Incremento2.model.Utilizador;
 import Equipa2.Incremento2.model.dto.LoginRequestDTO;
 import Equipa2.Incremento2.model.dto.ServicoDTO;
+import Equipa2.Incremento2.model.dto.UtilizadorDTO;
 import Equipa2.Incremento2.service.UtilizadorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,10 +21,13 @@ public class AuthController {
     @PostMapping
     public ResponseEntity<?> login(@RequestBody LoginRequestDTO loginRequest){
         Utilizador utilizador = utilizadorService.findByEmail(loginRequest.getEmail());
+        UtilizadorDTO userDto = utilizadorService.findDTOById(utilizador.getId());
+
+
         if(utilizador == null || !loginRequest.getPassword().equals(utilizador.getPassword())){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
         }
 
-        return ResponseEntity.ok().body(utilizador);
+        return ResponseEntity.ok().body(userDto);
     }
 }

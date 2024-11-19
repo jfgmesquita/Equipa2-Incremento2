ac2f43a580f3ae9e47967daff55b478f57984cb0
Devysson Cardoso <devyssonsc@gmail.com>
2024-11-19 15:40:11 +0000

    Operações com solicitação OK

--- src/main/java/Equipa2/Incremento2/controller/SolicitacaoController.java
+++ src/main/java/Equipa2/Incremento2/controller/SolicitacaoController.java
@@ -7,16 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import Equipa2.Incremento2.model.Cliente;
-import Equipa2.Incremento2.model.Profissional;
-import Equipa2.Incremento2.model.Servico;
+import Equipa2.Incremento2.model.*;
 import Equipa2.Incremento2.model.dto.SolicitacaoDTO;
+import Equipa2.Incremento2.model.enums.UserType;
+import Equipa2.Incremento2.service.ServicoService;
 import Equipa2.Incremento2.service.UtilizadorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import Equipa2.Incremento2.model.Solicitacao;
 import Equipa2.Incremento2.service.SolicitacaoService;
 
 @RestController
@@ -29,6 +28,47 @@ public class SolicitacaoController {
     @Autowired
     private UtilizadorService utilizadorService;
 
+    @Autowired
+    private ServicoService servicoService;
+
+    @GetMapping("/utilizador/{utilizadorId}")
+    public ResponseEntity<?> getAllSolicitacoesByUtilizadorId(@PathVariable UUID utilizadorId){
+        Utilizador uti = utilizadorService.findById(utilizadorId);
+
+        List<Solicitacao> solicitacoes;
+
+        if(uti.getUserType() == UserType.CLIENTE){
+            Cliente cli = (Cliente) uti;
+            solicitacoes = cli.getSolicitacoes();
+        } else if(uti.getUserType() == UserType.PROFISSIONAL){
+            Profissional pro = (Profissional) uti;
+            solicitacoes = pro.getSolicitacoes();
+        } else{
+            return ResponseEntity.badRequest().body("Utilizadores do tipo ADMIN não tem solicitações");
+        }
+
+        List<SolicitacaoDTO> dtos = new ArrayList<>();
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+
+        for(Solicitacao sol : solicitacoes){
+            String formattedDate = sol.getData().format(dtf);
+
+            dtos.add(
+                    new SolicitacaoDTO(
+                            sol.getId(),
+                            sol.getStatus(),
+                            utilizadorService.findDTOById(sol.getCliente().getId()),
+                            servicoService.findDTOById(sol.getServico().getId()),
+                            formattedDate
+                    )
+            );
+        }
+
+        return ResponseEntity.ok().body(dtos);
+
+    }
+
     @GetMapping
     public ResponseEntity<List<SolicitacaoDTO>> getAllSolicitacoes() {
         List<Solicitacao> solicitacoes = solicitacaoService.findAll();
@@ -38,14 +78,14 @@ public class SolicitacaoController {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
         for(Solicitacao sol : solicitacoes){
-            String formattedDate = dtf.format(sol.getData());
+            String formattedDate = sol.getData().format(dtf);
 
             dtos.add(
                     new SolicitacaoDTO(
                             sol.getId(),
                             sol.getStatus(),
                             utilizadorService.findDTOById(sol.getCliente().getId()),
-                            utilizadorService.findDTOById(sol.getProfissional().getId()),
+                            servicoService.findDTOById(sol.getServico().getId()),
                             formattedDate
                     )
             );
@@ -53,6 +93,7 @@ public class SolicitacaoController {
 
         return ResponseEntity.ok().body(dtos);
     }
+
 
     @GetMapping("/{id}")
     public ResponseEntity<Solicitacao> getSolicitacaoById(@PathVariable UUID id) {
@@ -68,11 +109,13 @@ public class SolicitacaoController {
     @PostMapping
     public ResponseEntity<?> createSolicitacao(@RequestBody SolicitacaoDTO solicitacao) {
 
-        Profissional pro = (Profissional) utilizadorService.findById(solicitacao.getProfissional().getId());
+        Servico ser = (Servico) servicoService.findById(solicitacao.getServico().getId());
+        Profissional pro = (Profissional) utilizadorService.findById(ser.getProfissional().getId());
         Cliente cli = (Cliente) utilizadorService.findById(solicitacao.getCliente().getId());
 
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
         LocalDateTime formattedDate;
+
         try {
             formattedDate = LocalDateTime.parse(solicitacao.getData(), dtf);
         } catch (DateTimeParseException e) {
@@ -83,13 +126,14 @@ public class SolicitacaoController {
         sol.setStatus(solicitacao.getStatus());
         sol.setCliente(cli);
         sol.setProfissional(pro);
+        sol.setServico(ser);
         sol.setData(formattedDate);
 
         solicitacaoService.save(sol);
 
         solicitacao.setId(sol.getId());
         solicitacao.setCliente(utilizadorService.findDTOById(cli.getId()));
-        solicitacao.setProfissional(utilizadorService.findDTOById(pro.getId()));
+        solicitacao.setServico(servicoService.findDTOById(ser.getId()));
 
         return ResponseEntity.ok().body(solicitacao);
     }
--- src/main/java/Equipa2/Incremento2/model/Solicitacao.java
+++ src/main/java/Equipa2/Incremento2/model/Solicitacao.java
@@ -7,6 +7,7 @@ import Equipa2.Incremento2.model.enums.StatusServico;
 import java.time.LocalDateTime;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
@@ -14,6 +15,7 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "solicitacao_de_servico")
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -26,57 +28,21 @@ public class Solicitacao {
     @Enumerated(EnumType.STRING)
     private StatusServico status;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name = "profissional_id")
     private Profissional profissional;
 
-    @Column(name = "morada")
-    private String morada;
+    @ManyToOne
+    @JoinColumn(name = "servico_id")
+    private Servico servico;
 
 	// @OneToOne(cascade = CascadeType.ALL)
 	// private Pagamento pagamento;
 
     @Column(name = "data")
     private LocalDateTime data;
-  
-    /**
-     * Construtor que inicializa uma nova solicitação de serviço com os dados fornecidos.
-     * 
-     * @param cliente		Cliente que solicitou.
-     * @param profissional  Profissional que aceitou o pedido.
-     * @param morada		Endereço do local.
-     * @param data			Data e horas.
-     */
-    public Solicitacao(Cliente cliente, Profissional profissional, LocalDateTime data) {
-        this.cliente = cliente;
-        this.profissional = profissional;
-        this.morada = cliente.getMorada();
-        status = StatusServico.PENDENTE;
-        this.data = data;
-    }
-
-    /**
-     * Construtor vazio para a classe Solicitacao.
-     * Inicializa todas variáveis de objeto como null.
-     */
-    public Solicitacao() {}
-
-    /**
-     * Retorna uma representação em string da solicitação.
-     *
-     * @return uma string que representa a solicitação
-     */
-    @Override
-    public String toString() {
-        return "ID: " + id + "\n" +
-            "Status: " + status + "\n" +
-            "Morada: " + morada + "\n" +
-            "Data: " + data + "\n" +
-            "Cliente: " + cliente.getNome() + "\n" +
-            "Profissional: " + profissional.getNome();
-    }
 }

987d721e561d3c2c1bade2b91aa8cc6d5b06e706
João Mesquita <jolitas2005@gmail.com>
2024-11-19 18:38:27 +0000

    Missing Repository Annotations

--- src/main/java/Equipa2/Incremento2/repository/ClienteRepository.java
+++ src/main/java/Equipa2/Incremento2/repository/ClienteRepository.java
@@ -3,6 +3,7 @@ package Equipa2.Incremento2.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import Equipa2.Incremento2.model.Cliente;
 
@@ -10,4 +11,5 @@ import Equipa2.Incremento2.model.Cliente;
  * Repositório para a entidade Cliente.
  * Fornece operações de acesso a dados para a entidade Cliente.
  */
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, UUID> {}
--- src/main/java/Equipa2/Incremento2/repository/ProfissionalRepository.java
+++ src/main/java/Equipa2/Incremento2/repository/ProfissionalRepository.java
@@ -3,6 +3,7 @@ package Equipa2.Incremento2.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import Equipa2.Incremento2.model.Profissional;
 
@@ -10,4 +11,5 @@ import Equipa2.Incremento2.model.Profissional;
  * Repositório para a entidade Profissional.
  * Fornece operações de acesso a dados para a entidade Profissional.
  */
+@Repository
 public interface ProfissionalRepository extends JpaRepository<Profissional, UUID> {}
